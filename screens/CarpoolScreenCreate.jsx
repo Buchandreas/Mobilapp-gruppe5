f35f46947c572a8d32d2c3b4fde734652524f6c3
Buchandreas <43332738+Buchandreas@users.noreply.github.com>
2024-09-05 14:24:39 +0200

    Translated carpoolcreate to english

--- screens/CarpoolScreenCreate.jsx
+++ screens/CarpoolScreenCreate.jsx
@@ -15,37 +15,37 @@ const CarpoolScreenCreate = () => {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>Start din fælleskørsel</Text>
+      <Text style={styles.title}>Start your carpooling</Text>
 
       <Text style={styles.paragraph}>
-        Opret din sammenkørsel og del din rute med andre. Spar penge, mød nye mennesker, og rejs grønnere.
+        Create your carpool and share your route with others. Save money, meet new people, and travel greener.
       </Text>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.inputTitle}>Tidspunkt</Text>
+        <Text style={styles.inputTitle}>Time</Text>
         <TextInput
           style={styles.input}
-          placeholder="Indtast tidspunkt"
+          placeholder="Time of departure"
           value={input1}
           onChangeText={setInput1}
         />
       </View>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.inputTitle}>Pris pr. passager</Text>
+        <Text style={styles.inputTitle}>Price per passenger</Text>
         <TextInput
           style={styles.input}
-          placeholder="Indtast pris"
+          placeholder="How much"
           value={input2}
           onChangeText={setInput2}
         />
       </View>
 
       <View style={styles.inputContainer}>
-        <Text style={styles.inputTitle}>Antal passagerer</Text>
+        <Text style={styles.inputTitle}>Number of passengers</Text>
         <TextInput
           style={styles.input}
-          placeholder="Indtast antal"
+          placeholder="How many"
           value={input3}
           onChangeText={setInput3}
         />
@@ -53,7 +53,7 @@ const CarpoolScreenCreate = () => {
 
       {/* Button to navigate to another screen */}
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('CarpoolHomeScreen')}>
-        <Text style={styles.buttonText}>Find other Carpools</Text>
+        <Text style={styles.buttonText}>Create carpool</Text>
       </TouchableOpacity>
     </ScrollView>
   );
@@ -87,6 +87,20 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 5,
+  },
+  button: {
+    backgroundColor: '#3D52D5',
+    paddingVertical: 15,
+    marginHorizontal: 50,
+    marginBottom: 10,
+    borderRadius: 24,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
